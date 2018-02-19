0c67316eae6946ed2f5292385946ee35b1f57b6e
350401 <pavan.rankireddy@gmail.com>
2018-02-19 17:47:31 +0100

    /create added

--- src/main/java/com/mkyong/WelcomeController.java
+++ src/main/java/com/mkyong/WelcomeController.java
@@ -1,13 +1,19 @@
 package com.mkyong;
 
 import com.mkyong.builder.Builder;
+import com.mkyong.data.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -34,6 +40,16 @@ public class WelcomeController {
          builder.build(null);
          logger.debug("Welcome {}, {}", app);
          return "welcome";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity checkPersonInfo(@Valid Employee employee, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.hasErrors());
+        }
+
+        return return ResponseEntity.ok("Employee Success");
     }
 
 }
